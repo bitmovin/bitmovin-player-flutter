18ca09522f64ed6e54417b398023fc79f5c7cd8b
Mario Graf <mario.graf@bitmovin.com>
2023-06-27 15:24:15 +0200

    Refactor to use CallbackToFutureAdapter

--- android/src/main/kotlin/com/bitmovin/player/drm/WidevineCallbacksHandler.kt
+++ android/src/main/kotlin/com/bitmovin/player/drm/WidevineCallbacksHandler.kt
@@ -1,24 +1,19 @@
 package com.bitmovin.player.drm
 
 import android.util.Base64
+import androidx.concurrent.futures.CallbackToFutureAdapter
 import com.bitmovin.core.data.Methods
 import com.bitmovin.core.runOnMainThread
 import com.bitmovin.player.api.drm.PrepareLicenseCallback
 import com.bitmovin.player.api.drm.PrepareMessageCallback
 import com.bitmovin.player.api.drm.WidevineConfig
 import io.flutter.plugin.common.MethodChannel
-import java.util.concurrent.locks.ReentrantLock
-import kotlin.concurrent.withLock
 
 class WidevineCallbacksHandler(
     private val metadata: WidevineConfigMetadata,
     private val widevineConfig: WidevineConfig,
     private val methodChannel: MethodChannel,
 ) {
-    private val lock = ReentrantLock()
-    private val prepareMessageCondition = lock.newCondition()
-    private val prepareLicenseCondition = lock.newCondition()
-
     init {
         assignHandlers()
     }
@@ -26,96 +21,59 @@ class WidevineCallbacksHandler(
     private fun assignHandlers() {
         if (metadata.hasPrepareMessage) {
             widevineConfig.prepareMessageCallback = PrepareMessageCallback { keyMessage ->
-                handlePrepareMessage(keyMessage)
+                handleCallback(
+                    Methods.WIDEVINE_PREPARE_MESSAGE,
+                    mapOf(
+                        "keyMessage" to Base64.encodeToString(keyMessage, Base64.NO_WRAP),
+                    ),
+                )
             }
         }
 
         if (metadata.hasPrepareLicense) {
             widevineConfig.prepareLicenseCallback = PrepareLicenseCallback { licenseResponse ->
-                handlePrepareLicense(licenseResponse)
-            }
-        }
-    }
-
-    private fun handlePrepareMessage(keyMessage: ByteArray): ByteArray {
-        var prepareMessageResult = keyMessage
-
-        lock.withLock {
-            runOnMainThread {
-                methodChannel.invokeMethod(
-                    Methods.WIDEVINE_PREPARE_MESSAGE,
-                    mapOf(
-                        "keyMessage" to Base64.encodeToString(keyMessage, Base64.NO_WRAP),
-                    ),
-                    object : MethodChannel.Result {
-                        override fun success(result: Any?) {
-                            if (result !is String) { return }
-
-                            lock.withLock {
-                                prepareMessageResult = Base64.decode(result, Base64.NO_WRAP)
-                                prepareMessageCondition.signal()
-                            }
-                        }
-
-                        override fun error(errorCode: String, errorMessage: String?, errorDetails: Any?) {
-                            lock.withLock {
-                                prepareMessageCondition.signal()
-                            }
-                        }
-
-                        override fun notImplemented() {
-                            lock.withLock {
-                                prepareMessageCondition.signal()
-                            }
-                        }
-                    },
-                )
-            }
-
-            prepareMessageCondition.await()
-        }
-
-        return prepareMessageResult
-    }
-
-    private fun handlePrepareLicense(licenseResponse: ByteArray): ByteArray {
-        var prepareLicenseResult = licenseResponse
-
-        lock.withLock {
-            runOnMainThread {
-                methodChannel.invokeMethod(
+                handleCallback(
                     Methods.WIDEVINE_PREPARE_LICENSE,
                     mapOf(
                         "licenseResponse" to Base64.encodeToString(licenseResponse, Base64.NO_WRAP),
                     ),
+                )
+            }
+        }
+    }
+
+    private fun handleCallback(methodName: String, arguments: Map<String, Any>): ByteArray {
+        return CallbackToFutureAdapter.getFuture { completer ->
+            runOnMainThread {
+                methodChannel.invokeMethod(
+                    methodName,
+                    arguments,
                     object : MethodChannel.Result {
                         override fun success(result: Any?) {
-                            if (result !is String) { return }
-
-                            lock.withLock {
-                                prepareLicenseResult = Base64.decode(result, Base64.NO_WRAP)
-                                prepareLicenseCondition.signal()
+                            if (result !is String) {
+                                completer.setException(
+                                    Exception("Invalid result type found for: $methodName"),
+                                )
+                                return
                             }
+
+                            completer.set(Base64.decode(result, Base64.NO_WRAP))
                         }
 
                         override fun error(errorCode: String, errorMessage: String?, errorDetails: Any?) {
-                            lock.withLock {
-                                prepareLicenseCondition.signal()
-                            }
+                            completer.setException(
+                                Exception("Error when calling $methodName. Error code: $errorCode, message: $errorMessage"),
+                            )
                         }
 
                         override fun notImplemented() {
-                            lock.withLock {
-                                prepareLicenseCondition.signal()
-                            }
+                            completer.setException(
+                                Exception("Method not implemented: $methodName"),
+                            )
                         }
                     },
                 )
             }
-
-            prepareLicenseCondition.await()
-        }
-
-        return prepareLicenseResult
+        }.get()
     }
 }
