dd918f697d3984216b30a16e3020fdabe47967a2
Mario Graf <mario.graf@bitmovin.com>
2023-06-28 08:46:01 +0200

    Add more doc

--- lib/src/api/player/player_config.dart
+++ lib/src/api/player/player_config.dart
@@ -1,11 +1,11 @@
-import 'package:bitmovin_player/src/api/licensing_config.dart';
-import 'package:bitmovin_player/src/api/playback_config.dart';
-import 'package:bitmovin_player/src/api/style_config.dart';
+import 'package:bitmovin_player/bitmovin_player.dart';
 import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'player_config.g.dart';
 
+/// Configures a new [Player] instance.
+/// Must not be modified after it was used to create a [Player] instance.
 @JsonSerializable(explicitToJson: true)
 class PlayerConfig extends Equatable {
   const PlayerConfig({
@@ -19,15 +19,22 @@ class PlayerConfig extends Equatable {
     return _$PlayerConfigFromJson(json);
   }
 
+  /// A Bitmovin player license key that can be found in the Bitmovin dashboard.
   @JsonKey(name: 'key')
   final String? key;
 
+  /// Configures visual presentation and behavior of the Player UI.
+  /// A default [StyleConfig] is used if not set here.
   @JsonKey(name: 'styleConfig')
   final StyleConfig? styleConfig;
 
+  /// Configures playback behavior.
+  /// A default [PlaybackConfig] is used if not set here.
   @JsonKey(name: 'playbackConfig')
   final PlaybackConfig? playbackConfig;
 
+  /// Configures license evaluation.
+  /// A default [LicensingConfig] is used if not set here.
   @JsonKey(name: 'licensingConfig')
   final LicensingConfig? licensingConfig;
 
--- lib/src/api/style_config.dart
+++ lib/src/api/style_config.dart
@@ -4,6 +4,7 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'style_config.g.dart';
 
+/// Configures visual presentation and behaviour of the Player UI.
 @JsonSerializable(explicitToJson: true)
 class StyleConfig extends Equatable {
   const StyleConfig({
@@ -19,21 +20,36 @@ class StyleConfig extends Equatable {
     return _$StyleConfigFromJson(json);
   }
 
+  /// Whether the Player UI is enabled.
+  /// Default value is `true`.
   @JsonKey(name: 'isUiEnabled', defaultValue: true)
   final bool isUiEnabled;
 
+  /// Optional URI pointing to the CSS file that should be used for the
+  /// Player UI instead of the default CSS file.
   @JsonKey(name: 'playerUiCss', defaultValue: null)
   final String? playerUiCss;
 
+  /// Optional URI pointing to the JS file that should be used for the
+  /// Player UI instead of the default JS file.
   @JsonKey(name: 'playerUiJs', defaultValue: null)
   final String? playerUiJs;
 
+  /// Optional URI pointing to the supplemental CSS file that will be used for
+  /// the Player UI. The contained styles will be added to the CSS file
+  /// specified in [playerUiCss].
   @JsonKey(name: 'supplementalPlayerUiCss', defaultValue: null)
   final String? supplementalPlayerUiCss;
 
+  /// Whether the first frame of the main content will be rendered before
+  /// playback starts.
+  /// Default value is `false`.
   @JsonKey(name: 'isHideFirstFrame', defaultValue: null)
   final bool isHideFirstFrame;
 
+  /// Specifies how the video content is scaled or stretched within the parent
+  /// container's bounds.
+  /// Default value is [ScalingMode.fit].
   @JsonKey(name: 'scalingMode', defaultValue: null)
   final ScalingMode scalingMode;
 
