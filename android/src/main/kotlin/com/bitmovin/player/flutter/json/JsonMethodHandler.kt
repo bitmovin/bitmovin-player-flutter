9b8ef76efee2699c471b27e97ace4cd9f4bc5eed
Kevin Rocard <kevin.rocard@bitmovin.com>
2023-06-29 20:44:22 +0200

    Add documentation

--- android/src/main/kotlin/com/bitmovin/player/flutter/json/JsonMethodHandler.kt
+++ android/src/main/kotlin/com/bitmovin/player/flutter/json/JsonMethodHandler.kt
@@ -3,6 +3,7 @@ package com.bitmovin.player.flutter.json
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 
+/** set Result from returned object or set an error if an exception is thrown. */
 internal class JsonMethodHandler(private val handler: (String, JMethodArgs) -> Any) :
     MethodChannel.MethodCallHandler {
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
