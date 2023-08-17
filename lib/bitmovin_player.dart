1d1fc90c71116fcae6020d7747fcf21728c4440b
Mario Graf <mario.graf@bitmovin.com>
2023-08-17 11:44:47 +0200

    Add FullscreenHandler in Dart

--- lib/bitmovin_player.dart
+++ lib/bitmovin_player.dart
@@ -36,6 +36,7 @@ export 'src/api/source/source_config.dart';
 export 'src/api/source/source_options.dart';
 export 'src/api/source/source_type.dart';
 export 'src/api/source/timeline_reference_point.dart';
+export 'src/api/ui/fullscreen_handler.dart';
 export 'src/api/ui/scaling_mode.dart';
 export 'src/api/ui/style_config.dart';
 export 'src/player.dart';
