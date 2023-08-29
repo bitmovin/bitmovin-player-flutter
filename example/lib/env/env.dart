9380f0bb1c848dbc00e0e4126c913a4fdc773505
Lucas <lucas.gruenberger@bitmovin.com>
2023-08-29 13:25:38 +0200

    Add additional env parameter to specify the Analytics LicenseKey
    [PFL-33]

--- example/lib/env/env.dart
+++ example/lib/env/env.dart
@@ -6,10 +6,16 @@ part 'env.g.dart';
 /// which contains the bitmovin player license key as
 /// `BITMOVIN_PLAYER_LICENSE_KEY=YOUR_LICENSE_KEY` where `YOUR_LICENSE_KEY`
 /// is your private bitmovin player license key.
+/// Optionally also add the `BITMOVIN_ANALYTICS_LICENSE_KEY=YOUR_LICENSE_KEY`
+/// where `YOUR_LICENSE_KEY` is your private bitmovin analytics license key.
 /// Then run `flutter pub run build_runner build --delete-conflicting-outputs`
 /// in the project root to generate the `env.g.dart` file.
 @Envied(path: '.env')
 abstract class Env {
   @EnviedField(varName: 'BITMOVIN_PLAYER_LICENSE_KEY')
   static const String bitmovinPlayerLicenseKey = _Env.bitmovinPlayerLicenseKey;
+
+  @EnviedField(varName: 'BITMOVIN_ANALYTICS_LICENSE_KEY')
+  static const String bitmovinAnalyticsLicenseKey =
+      _Env.bitmovinAnalyticsLicenseKey;
 }
