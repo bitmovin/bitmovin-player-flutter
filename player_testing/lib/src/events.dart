074802d46b57bd730436c811ea180fba59c2c591
Mario Graf <mario.graf@bitmovin.com>
2023-08-30 14:13:06 +0200

    Add new event types

--- player_testing/lib/src/events.dart
+++ player_testing/lib/src/events.dart
@@ -3,6 +3,8 @@ import 'package:bitmovin_player/bitmovin_player.dart';
 typedef E = Events;
 
 abstract class Events {
+  static const play = PlayEvent(time: 0, timestamp: 0);
+  static const playing = PlayingEvent(time: 0, timestamp: 0);
   static const ready = ReadyEvent(timestamp: 0);
   static const timeShift = TimeShiftEvent(position: 0, target: 0, timestamp: 0);
   static const timeShifted = TimeShiftedEvent(timestamp: 0);
