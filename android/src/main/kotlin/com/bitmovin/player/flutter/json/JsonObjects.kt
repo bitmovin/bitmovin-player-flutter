bitmovin/bitmovin-player-flutter

commit d4a6338b23dbd898cfde76fe2cc57d79ac391da5

Updates according to PR comments
Use empty (new) SourceMetadata for Source.create call
Update enumGetter function call
Remove unnecessary comment

[PFL-33]

android/src/main/kotlin/com/bitmovin/player/flutter/json/JsonObjects.kt
@@ -215,8 +215,8 @@ private fun <T> Getter<T?>.require() = Getter<T> { thisRef, property ->
 
 private inline fun <reified T> listGetter() = getter { list: List<*> -> list.map { it as T } }
 
-private inline fun <reified E : Enum<E>> enumGetter(crossinline customEnumValue: (stringValue: String) -> E) =
-    getter { name: String -> customEnumValue(name) }
+private inline fun <reified E : Enum<E>> enumGetter(crossinline customEnumValue: (enumValueOf: String) -> E) =
+    getter(enumValueOf)
 
 private inline fun <reified E : Enum<E>> enumGetter() =
     getter { name: String -> enumValueOf<E>(name, ignoreCase = true) }

android/src/main/kotlin/com/bitmovin/player/flutter/FlutterPlayer.kt
@@ -3,6 +3,7 @@ package com.bitmovin.player.flutter
 import android.content.Context
 import com.bitmovin.analytics.api.AnalyticsConfig
 import com.bitmovin.analytics.api.DefaultMetadata
+import com.bitmovin.analytics.api.SourceMetadata
 import com.bitmovin.player.api.Player
 import com.bitmovin.player.api.PlayerConfig
 import com.bitmovin.player.api.analytics.AnalyticsApi.Companion.analytics
@@ -63,12 +64,8 @@ class FlutterPlayer(
             )
         }
 
-        val sourceMetadata = jSourceConfig.analyticsSourceMetadata?.toNative()
-        if (sourceMetadata != null) {
-            load(Source.create(sourceConfig, sourceMetadata))
-        } else {
-            load(Source.create(sourceConfig))
-        }
+        val sourceMetadata = jSourceConfig.analyticsSourceMetadata?.toNative() ?: SourceMetadata()
+        load(Source.create(sourceConfig, sourceMetadata))
     }
 
     private fun Player.onMethodCall(method: String, arg: JPlayerMethodArg): Any = when (method) {

lib/src/player.dart
@@ -205,8 +205,6 @@ class Player with PlayerEventHandler implements PlayerApi {
 }
 
 class _AnalyticsApi implements AnalyticsApi {
-  // Dart does not support inner classes,
-  // so we need to pass the player as parameter
   _AnalyticsApi(this._player);
 
   final Player _player;

commit 1b6039c0340ff8fa85753fa04224c7966fbb5bdd

Fix compiler error introduced with d4a6338b23dbd898cfde76fe2cc57d79ac391da5

android/src/main/kotlin/com/bitmovin/player/flutter/json/JsonObjects.kt
@@ -215,7 +215,7 @@ private fun <T> Getter<T?>.require() = Getter<T> { thisRef, property ->
 
 private inline fun <reified T> listGetter() = getter { list: List<*> -> list.map { it as T } }
 
-private inline fun <reified E : Enum<E>> enumGetter(crossinline customEnumValue: (enumValueOf: String) -> E) =
+private inline fun <reified E : Enum<E>> enumGetter(crossinline enumValueOf: (String) -> E) =
     getter(enumValueOf)
 
 private inline fun <reified E : Enum<E>> enumGetter() =