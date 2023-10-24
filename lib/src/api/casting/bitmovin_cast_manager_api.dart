37f96c4cb0803888875bb3e64f6a0de1294bcd4a
Kevin Rocard <kevin.rocard@bitmovin.com>
2023-10-24 12:04:58 +0200

    Revert "Fix lint warnings"
    This reverts commit 927706fb031925a07ace0b0053928a083027631e.

--- lib/src/api/casting/bitmovin_cast_manager_api.dart
+++ lib/src/api/casting/bitmovin_cast_manager_api.dart
@@ -1,7 +1,6 @@
 import 'package:bitmovin_player/bitmovin_player.dart';
 
 /// Provides access to GoogleCast related features of [Player]s.
-// ignore: one_member_abstracts
 abstract class BitmovinCastManagerApi {
   /// Sends the given `message` to the cast receiver.
   Future<void> sendMessage({
