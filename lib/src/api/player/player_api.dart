23de22174eeff62583e9a2cf2b6d49632c5a8373
Michele Pozzi <123.mpozzi@gmail.com>
2023-11-23 13:57:22 +0100

    feat(airplay): add AirPlay in Player API

--- lib/src/api/player/player_api.dart
+++ lib/src/api/player/player_api.dart
@@ -101,4 +101,19 @@ abstract class PlayerApi {
 
   /// Stops casting the current video.
   Future<void> castStop();
+
+  /// Returns `true` when media is played externally using AirPlay.
+  ///
+  /// Only available on iOS. Result in a no-op on tvOS.
+  Future<bool> get isAirPlayActive;
+
+  /// Returns `true` when AirPlay is available.
+  ///
+  /// Only available on iOS. Result in a no-op on tvOS.
+  Future<bool> get isAirPlayAvailable;
+
+  /// Shows the AirPlay playback target picker.
+  ///
+  /// Only available on iOS. Result in a no-op on tvOS.
+  Future<void> showAirPlayTargetPicker();
 }
