b6be5227f5150947f50edaae3d3dcefd5dfe6a76
Michele Pozzi <123.mpozzi@gmail.com>
2023-11-23 14:03:48 +0100

    feat(airplay): add AirPlay events

---
+++ lib/src/api/event/player/airplay_changed_event.dart
@@ -0,0 +1,32 @@
+import 'package:bitmovin_player/src/api/event/event.dart';
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'airplay_changed_event.g.dart';
+
+/// Emitted when AirPlay playback starts or stops.
+@JsonSerializable(explicitToJson: true)
+class AirPlayChangedEvent extends Event with EquatableMixin {
+  const AirPlayChangedEvent({
+    required super.timestamp,
+    required this.isAirPlayActive,
+    required this.time,
+  });
+
+  factory AirPlayChangedEvent.fromJson(Map<String, dynamic> json) {
+    return _$AirPlayChangedEventFromJson(json);
+  }
+
+  /// Indicates whether AirPlay is active.
+  final bool isAirPlayActive;
+
+  // The current playback time (in seconds).
+  @JsonKey(name: 'time')
+  final double time;
+
+  @override
+  Map<String, dynamic> toJson() => _$AirPlayChangedEventToJson(this);
+
+  @override
+  List<Object?> get props => [timestamp, isAirPlayActive, time];
+}
