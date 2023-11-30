17e6336bd9698fca8bfcfe27cd0d36448e650315
Mario Graf <mario.graf@bitmovin.com>
2023-11-30 14:05:49 +0100

    feat(pip-ios-api): fix Swift coding style

--- ios/Classes/Methods.swift
+++ ios/Classes/Methods.swift
@@ -35,10 +35,10 @@ internal enum Methods {
     static let destroyPlayerView = "destroyPlayerView"
     static let enterFullscreen = "enterFullscreen"
     static let exitFullscreen = "exitFullscreen"
-    static let isPictureInPicture = "isPictureInPicture";
-    static let isPictureInPictureAvailable = "isPictureInPictureAvailable";
-    static let enterPictureInPicture = "enterPictureInPicture";
-    static let exitPictureInPicture = "exitPictureInPicture";
+    static let isPictureInPicture = "isPictureInPicture"
+    static let isPictureInPictureAvailable = "isPictureInPictureAvailable"
+    static let enterPictureInPicture = "enterPictureInPicture"
+    static let exitPictureInPicture = "exitPictureInPicture"
 
     // Fairplay related methods
     static let fairplayPrepareMessage = "fairplayPrepareMessage"
