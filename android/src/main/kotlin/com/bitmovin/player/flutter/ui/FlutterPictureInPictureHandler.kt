5f88ad994497b272a6fb29a4d7543fba658ecdd1
Vehovec <ziga.veho@gmail.com>
2023-12-19 17:24:55 +0100

    chore: start documenting special handling

--- android/src/main/kotlin/com/bitmovin/player/flutter/ui/FlutterPictureInPictureHandler.kt
+++ android/src/main/kotlin/com/bitmovin/player/flutter/ui/FlutterPictureInPictureHandler.kt
@@ -8,6 +8,13 @@ class FlutterPictureInPictureHandler(
     activity: Activity,
     player: Player,
 ) : DefaultPictureInPictureHandler(activity, player) {
+    // Track this state manually, since the lifecycle in Flutter is a bit different than using a
+    // DefaultPictureInPictureHandler within the native app environment.
+    // The difference being that we always want to emit PictureInPictureEnter and PictureInPictureExit events
+    // so that they can bubble up to the flutter UI and customers can react to picture in picture state changes.
+    // The mentioned events are only emitted if not already in the same `isPictureInPicture` state and if called
+    // the PiP is exited via palyerView API.
+    // TODO: Improve this comment
     private var _isPictureInPicture = false
 
     override val isPictureInPicture: Boolean
