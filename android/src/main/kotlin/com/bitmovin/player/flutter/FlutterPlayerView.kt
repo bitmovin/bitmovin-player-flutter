b14d428d746e81dc13084ebbb6f77079c4eafcba
Vehovec <ziga.veho@gmail.com>
2023-12-21 14:18:40 +0100

    chore: update documentation

--- android/src/main/kotlin/com/bitmovin/player/flutter/FlutterPlayerView.kt
+++ android/src/main/kotlin/com/bitmovin/player/flutter/FlutterPlayerView.kt
@@ -123,12 +123,14 @@ class FlutterPlayerView(
         var isInPictureInPictureMode = activity.isInPictureInPictureMode
         // Listening to PiP changes usually happens by overriding `onPictureInPictureModeChanged` in the activity.
         // This is not doable in an SDK context since the library consumer controls the activity.
+        // It's also possible to set a `addOnPictureInPictureModeChangedListener` to the activity that
+        // implements `OnPictureInPictureModeChangedProvider`, unfortunately `FlutterActvity` doesn't implement it.
         // To work around this limitation we listen to configuration changes and check if the PiP mode changed.
         // Since Flutter's `PlatformView` isn't actually an Android `View`,
         // we unfortunately cannot just override the `onConfigurationChanged` function.
         // Instead, we add a blank `View` to the `PlayerView` and override its `onConfigurationChanged` function.
-        // The alternative would be registering a `ComponentCallbacks` object to the activity, which unfortunately doesn't
-        // work for some Android versions (tested on Android 10 with FlutterFragmentActivity).
+        // The alternative would be registering a `ComponentCallbacks` object to the activity,
+        // which doesn't work for some Android versions (tested on Android 10 with FlutterFragmentActivity).
         addView(
             object : View(context) {
                 override fun onConfigurationChanged(newConfig: Configuration) {
