18d41bf63197cf6f8f6f64e91c29e926c8be4774
Mario Graf <mario.graf@bitmovin.com>
2024-11-21 08:46:41 +0100

    Register view factory for web

--- lib/src/web/bitmovin_player.dart
+++ lib/src/web/bitmovin_player.dart
@@ -2,27 +2,40 @@
 // of your plugin as a separate package, instead of inlining it in the same
 // package as the core of your plugin.
 // ignore: avoid_web_libraries_in_flutter
-
 import 'dart:async';
+import 'dart:html';
+import 'dart:ui_web' as ui;
 
-import 'package:bitmovin_player/src/web/bitmovin_player_platform_interface.dart';
+import 'package:bitmovin_player/bitmovin_player.dart';
+import 'package:bitmovin_player/src/channels.dart';
+import 'package:bitmovin_player/src/platform/bitmovin_player_platform_interface.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 /// A web implementation of the BitmovinPlayerPlatform of the BitmovinPlayer
 /// plugin.
 class BitmovinPlayerWeb extends BitmovinPlayerPlatform {
   /// Constructs a BitmovinPlayerWeb
-  BitmovinPlayerWeb();
+  BitmovinPlayerWeb() {
+    ui.platformViewRegistry.registerViewFactory(Channels.playerView,
+        (int viewId, {Object? params}) {
+      final div = document.createElement('div') as DivElement
+        ..id = (params as dynamic)['playerId'] as String
+        ..style.width = '100%'
+        ..style.height = '100%'
+        ..className = 'bitmovinplayer';
+      // TODO(mario): set player on view
+
+      return div;
+    });
+  }
 
   static void registerWith(Registrar registrar) {
     BitmovinPlayerPlatform.instance = BitmovinPlayerWeb();
   }
 
-  /// Returns a [String] containing the version of the platform.
   @override
-  Future<String?> getPlatformVersion() async {
-    // final version = web.window.navigator.userAgent;
-    // return version;
-    return null;
+  Future<bool> createPlayer(String id, PlayerConfig config) async {
+    // TODO(mario): Implement createPlayer for web.
+    return true;
   }
 }
